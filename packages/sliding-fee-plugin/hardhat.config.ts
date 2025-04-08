d1f517ecfdd97675e9a45ff696bccddb890297ca
debych <zh.eliseev@yandex.ru>
2025-04-08 18:45:40 +0300

    working tests on sliding-fee

--- packages/sliding-fee-plugin/hardhat.config.ts
+++ packages/sliding-fee-plugin/hardhat.config.ts
@@ -1,13 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition-ethers";
+import { SolcUserConfig } from 'hardhat/types';
 import baseConfig from '../../hardhat.base.config';
+
+const HIGHEST_OPTIMIZER_COMPILER_SETTINGS: SolcUserConfig = {
+  version: '0.8.20',
+  settings: {
+    evmVersion: 'paris',
+    optimizer: {
+      enabled: true,
+      runs: 1_000_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+};
 
 const config: HardhatUserConfig = {
   networks: baseConfig.networks,
   etherscan: baseConfig.etherscan,
   typechain: baseConfig.typechain,
-  solidity: "0.8.20",
+  solidity: HIGHEST_OPTIMIZER_COMPILER_SETTINGS
 };
 
 export default config;
---
+++ packages/test-utils/snapshotGasCost.ts
@@ -0,0 +1,35 @@
+import { TransactionReceipt, TransactionResponse } from '@ethersproject/abstract-provider';
+import { expect } from './expect';
+import { Contract, ContractTransactionResponse } from 'ethers';
+
+export default async function snapshotGasCost(
+  x:
+    | TransactionResponse
+    | Promise<TransactionResponse>
+    | ContractTransactionResponse
+    | Promise<ContractTransactionResponse>
+    | TransactionReceipt
+    | Promise<BigInt>
+    | BigInt
+    | Contract
+    | Promise<Contract>
+): Promise<void> {
+  const resolved = await x;
+  if (resolved instanceof BigInt || resolved instanceof Number || typeof resolved == 'number') {
+    expect(Number(resolved)).toMatchSnapshot();
+  } else if ('deploymentTransaction' in resolved) {
+    const receipt = await resolved.deploymentTransaction()?.wait();
+    if (receipt) {
+      expect(Number(receipt.gasUsed)).toMatchSnapshot();
+    } else {
+        console.warn('⚠️Deployment transaction returned null. Skipping gas usage snapshot.\n', new Error().stack);
+    }
+  } else if ('wait' in resolved) {
+    const waited = await resolved.wait();
+    if (waited) {
+        expect(Number(waited.gasUsed)).toMatchSnapshot();
+    } else {
+        console.warn('⚠️Transaction returned null. Skipping gas usage snapshot.\n', new Error().stack);
+    }
+  }
+}

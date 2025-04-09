44a23d7a65396f21088e41aa68f1bc67d989b5b0
debych <zh.eliseev@yandex.ru>
2025-04-09 20:25:24 +0300

    add base plugin

---
+++ packages/base-plugin/contracts/AlgebraBasePluginFactory.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.20;
+
+import './interfaces/IBasePluginFactory.sol';
+import '@cryptoalgebra/dynamic-fee-plugin/contracts/libraries/AdaptiveFee.sol';
+import './AlgebraBasePlugin.sol';
+
+/// @title Algebra Integral 1.2.1 default plugin factory
+/// @notice This contract creates Algebra adaptive fee plugins for Algebra liquidity pools
+/// @dev This plugin factory can only be used for Algebra base pools
+contract AlgebraBasePluginFactory is IBasePluginFactory {
+  /// @inheritdoc IBasePluginFactory
+  bytes32 public constant override ALGEBRA_BASE_PLUGIN_FACTORY_ADMINISTRATOR = keccak256('ALGEBRA_BASE_PLUGIN_FACTORY_ADMINISTRATOR');
+
+  /// @inheritdoc IBasePluginFactory
+  address public immutable override algebraFactory;
+
+  /// @inheritdoc IDynamicFeePluginFactory
+  AlgebraFeeConfiguration public override defaultFeeConfiguration; // values of constants for sigmoids in fee calculation formula
+
+  /// @inheritdoc IFarmingPluginFactory
+  address public override farmingAddress;
+
+  /// @inheritdoc IBasePluginFactory
+  mapping(address poolAddress => address pluginAddress) public override pluginByPool;
+
+  modifier onlyAdministrator() {
+    require(IAlgebraFactory(algebraFactory).hasRoleOrOwner(ALGEBRA_BASE_PLUGIN_FACTORY_ADMINISTRATOR, msg.sender), 'Only administrator');
+    _;
+  }
+
+  constructor(address _algebraFactory) {
+    algebraFactory = _algebraFactory;
+    defaultFeeConfiguration = AdaptiveFee.initialFeeConfiguration();
+    emit DefaultFeeConfiguration(defaultFeeConfiguration);
+  }
+
+  /// @inheritdoc IAlgebraPluginFactory
+  function beforeCreatePoolHook(address pool, address, address, address, address, bytes calldata) external override returns (address) {
+    require(msg.sender == algebraFactory);
+    return _createPlugin(pool);
+  }
+
+  /// @inheritdoc IAlgebraPluginFactory
+  function afterCreatePoolHook(address, address, address) external view override {
+    require(msg.sender == algebraFactory);
+  }
+
+  /// @inheritdoc IBasePluginFactory
+  function createPluginForExistingPool(address token0, address token1) external override returns (address) {
+    IAlgebraFactory factory = IAlgebraFactory(algebraFactory);
+    require(factory.hasRoleOrOwner(factory.POOLS_ADMINISTRATOR_ROLE(), msg.sender));
+
+    address pool = factory.poolByPair(token0, token1);
+    require(pool != address(0), 'Pool not exist');
+
+    return _createPlugin(pool);
+  }
+
+  function _createPlugin(address pool) internal returns (address) {
+    require(pluginByPool[pool] == address(0), 'Already created');
+    IDynamicFeeManager volatilityOracle = new AlgebraBasePlugin(pool, algebraFactory, address(this), defaultFeeConfiguration);
+    pluginByPool[pool] = address(volatilityOracle);
+    return address(volatilityOracle);
+  }
+
+  /// @inheritdoc IDynamicFeePluginFactory
+  function setDefaultFeeConfiguration(AlgebraFeeConfiguration calldata newConfig) external override onlyAdministrator {
+    AdaptiveFee.validateFeeConfiguration(newConfig);
+    defaultFeeConfiguration = newConfig;
+    emit DefaultFeeConfiguration(newConfig);
+  }
+
+  /// @inheritdoc IFarmingPluginFactory
+  function setFarmingAddress(address newFarmingAddress) external override onlyAdministrator {
+    require(farmingAddress != newFarmingAddress);
+    farmingAddress = newFarmingAddress;
+    emit FarmingAddress(newFarmingAddress);
+  }
+}

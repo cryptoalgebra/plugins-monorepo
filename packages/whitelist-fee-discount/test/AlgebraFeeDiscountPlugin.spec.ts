39256a528a538f8e601726543184b7c4cf0c606e
IliaAzhel <aje-ilia2013@yandex.by>
2025-04-18 12:20:09 +0300

    fix whitelist fee discount plugin test

--- packages/whitelist-fee-discount/test/AlgebraFeeDiscountPlugin.spec.ts
+++ packages/whitelist-fee-discount/test/AlgebraFeeDiscountPlugin.spec.ts
@@ -1,26 +1,18 @@
 import { Wallet, ZeroAddress } from 'ethers';
 import { ethers } from 'hardhat';
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
-import { expect } from './shared/expect';
-import { feeDiscountPluginFixture } from './shared/fixtures';
-import { PLUGIN_FLAGS, encodePriceSqrt, expandTo18Decimals, getMaxTick, getMinTick } from './shared/utilities';
 
-import { MockPool, AlgebraFeeDiscountPlugin, FeeDiscountPluginFactory, FeeDiscountRegistry, MockFactory } from '../typechain';
+import { expect } from 'test-utils/expect';
+import { PLUGIN_FLAGS, encodePriceSqrt} from 'test-utils/utilities';
 
-import snapshotGasCost from './shared/snapshotGasCost';
-import { feeDiscountPuginFactorySol } from '../typechain/factories/contracts';
+import { MockPool, TestFeeDiscountPlugin, FeeDiscountRegistry, MockFactory } from '../typechain';
 
 describe('AlgebraSecurityPlugin', () => {
   let wallet: Wallet, other: Wallet;
 
-  let plugin: AlgebraFeeDiscountPlugin; 
-  let mockPool: MockPool; // mock of AlgebraPool
-  let pluginFactory: FeeDiscountPluginFactory;
+  let plugin: TestFeeDiscountPlugin; 
+  let mockPool: MockPool; 
   let registry: FeeDiscountRegistry;
   let mockFactory: MockFactory;
-  async function initializeAtZeroTick(pool: MockPool) {
-    await pool.initialize(encodePriceSqrt(1, 1));
-  }
 
   before('prepare signers', async () => {
     [wallet, other] = await (ethers as any).getSigners();
@@ -28,63 +20,18 @@ describe('AlgebraSecurityPlugin', () => {
 
   beforeEach('deploy test AlgebaraSecurityPlugin', async () => {
     const mockFactoryFactory = await ethers.getContractFactory('MockFactory');
-    const mockFactory = (await mockFactoryFactory.deploy()) as any as MockFactory;
-
-    const pluginFactoryFactory = await ethers.getContractFactory('FeeDiscountPluginFactory');
-    const pluginFactory = (await pluginFactoryFactory.deploy(mockFactory)) as any as FeeDiscountPluginFactory;
+    mockFactory = (await mockFactoryFactory.deploy()) as any as MockFactory;
   
     const mockPoolFactory = await ethers.getContractFactory('MockPool');
-    const mockPool = (await mockPoolFactory.deploy()) as any as MockPool;
+    mockPool = (await mockPoolFactory.deploy()) as any as MockPool;
   
     const registryFactory = await ethers.getContractFactory('FeeDiscountRegistry');
-    const registry = (await registryFactory.deploy(mockFactory)) as any as FeeDiscountRegistry;
+    registry = (await registryFactory.deploy(mockFactory)) as any as FeeDiscountRegistry;
   
-    await pluginFactory.setFeeDiscountRegistry(registry)
-    await mockFactory.beforeCreatePoolHook(pluginFactory, mockPool);
-    const pluginAddress = await pluginFactory.pluginByPool(mockPool);
-  
-    const pluginContractFactory = await ethers.getContractFactory('AlgebraFeeDiscountPlugin');
-    const plugin = pluginContractFactory.attach(pluginAddress) as any as AlgebraFeeDiscountPlugin;
-  });
+    const pluginContractFactory = await ethers.getContractFactory('TestFeeDiscountPlugin');
+    plugin = (await pluginContractFactory.deploy(mockPool, mockFactory, wallet.address, registry)) as any as TestFeeDiscountPlugin;
 
-  // plain tests for hooks functionality
-  describe('#Hooks', () => {
-    it('only pool can call hooks', async () => {
-      const errorMessage = 'Only pool can call this';
-      await expect(plugin.beforeInitialize(wallet.address, 100)).to.be.revertedWith(errorMessage);
-      await expect(plugin.afterInitialize(wallet.address, 100, 100)).to.be.revertedWith(errorMessage);
-      await expect(plugin.beforeModifyPosition(wallet.address, wallet.address, 100, 100, 100, '0x')).to.be.revertedWith(errorMessage);
-      await expect(plugin.afterModifyPosition(wallet.address, wallet.address, 100, 100, 100, 100, 100, '0x')).to.be.revertedWith(errorMessage);
-      await expect(plugin.beforeSwap(wallet.address, wallet.address, true, 100, 100, false, '0x')).to.be.revertedWith(errorMessage);
-      await expect(plugin.afterSwap(wallet.address, wallet.address, true, 100, 100, 100, 100, '0x')).to.be.revertedWith(errorMessage);
-      await expect(plugin.beforeFlash(wallet.address, wallet.address, 100, 100, '0x')).to.be.revertedWith(errorMessage);
-      await expect(plugin.afterFlash(wallet.address, wallet.address, 100, 100, 100, 100, '0x')).to.be.revertedWith(errorMessage);
-    });
-
-    describe('not implemented hooks', async () => {
-      let defaultConfig: bigint;
-
-      beforeEach('connect plugin to pool', async () => {
-        defaultConfig = await plugin.defaultPluginConfig();
-        await mockPool.setPlugin(plugin);
-      });
-
-      it('resets config after afterModifyPosition', async () => {
-        await mockPool.initialize(encodePriceSqrt(1, 1));
-        await mockPool.setPluginConfig(PLUGIN_FLAGS.AFTER_POSITION_MODIFY_FLAG);
-        expect((await mockPool.globalState()).pluginConfig).to.be.eq(PLUGIN_FLAGS.AFTER_POSITION_MODIFY_FLAG);
-        await mockPool.mint(wallet.address, wallet.address, 0, 60, 100, '0x');
-        expect((await mockPool.globalState()).pluginConfig).to.be.eq(defaultConfig);
-      });
-
-      it('resets config after afterFlash', async () => {
-        await mockPool.setPluginConfig(PLUGIN_FLAGS.AFTER_FLASH_FLAG);
-        expect((await mockPool.globalState()).pluginConfig).to.be.eq(PLUGIN_FLAGS.AFTER_FLASH_FLAG);
-        await mockPool.flash(wallet.address, 100, 100, '0x');
-        expect((await mockPool.globalState()).pluginConfig).to.be.eq(defaultConfig);
-      });
-    });
-
+    await plugin.setFee(100);
   });
 
   describe('#FeeDiscountPlugin', () => {

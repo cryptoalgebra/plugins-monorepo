6a05ac8e520a572d6ce1237dfb426e26d0ea6326
fourlen <oborin.2013@mail.ru>
2025-04-28 14:31:59 +0000

    add deploy factory script

--- packages/default-alm-plugin/ignition/modules/deploy.ts
+++ packages/default-alm-plugin/ignition/modules/deploy.ts
@@ -2,14 +2,12 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const factory = "0x51a744E9FEdb15842c3080d0937C99A365C6c358";
 
-export default buildModule("RebalanceManagerOracle", (m) => {
-	const defaultAlmPlugin = m.contract("DefaultAlmPlugin", [factory]);
+export default buildModule("AlgebraDefaultPluginFactory", (m) => {
+	const defaultAlmPluginFactory = m.contract("AlgebraDefaultPluginFactory", [factory]);
 	
 	const factoryContract = m.contractAt("IAlgebraFactory", factory);
 
-	console.log('defaultAlmPlugin.id: ', defaultAlmPlugin.id);
+	m.call(factoryContract, "setDefaultPluginFactory", [defaultAlmPluginFactory]);
 
-	m.call(factoryContract.setDefaultPluginFactory, defaultAlmPlugin.id);
-
-	return { defaultAlmPlugin };
+	return { defaultAlmPluginFactory };
 });

417edc77ad50789fb16fbab50d697b29e967cca6
IliaAzhel <aje-ilia2013@yandex.by>
2025-06-20 10:52:38 +0300

    refund native token after payment in mint callback

--- packages/limit-order/contracts/base/LimitOrderPayments.sol
+++ packages/limit-order/contracts/base/LimitOrderPayments.sol
@@ -31,7 +31,9 @@ abstract contract LimitOrderPayments {
       // pull payment
       TransferHelper.safeTransferFrom(token, payer, recipient, value);
     }
+  }
 
+  function _refundNativeToken(address payer) internal {
     if (address(this).balance > 0) TransferHelper.safeTransferNative(payer, address(this).balance);
   }
 

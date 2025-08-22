7c60de983d55aeff215b084950071703cba93eb2
IliaAzhel <aje-ilia2013@yandex.by>
2025-08-22 16:17:28 +0300

    fix ReentrancyGuard import

--- packages/lotus/contracts/ReflexAfterSwap.sol
+++ packages/lotus/contracts/ReflexAfterSwap.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "./interfaces/IReflexRouter.sol";
 import "./base/FundsSplitter.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @title ReflexAfterSwap
 /// @notice Abstract contract that integrates with Reflex Router for post-swap profit extraction and distribution

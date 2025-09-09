f2e0ae051ac7dd8e20913b2b614bacc86776029f
IliaAzhel <aje-ilia2013@yandex.by>
2025-09-09 13:18:09 +0300

    add custom plugin factory and deploy scripts

---
+++ packages/default-plugin/ignition/modules/deployCustom.ts
@@ -0,0 +1,13 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const factoryAddress = "0x36077D39cdC65E1e3FB65810430E5b2c4D5fA29E";
+const farmingCenter = "0xB781A7afCf46dEC1Fa16a722eFD25433D1B9F261";
+const entryPoint = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+
+export default buildModule("AlgebraCustomPluginFactory", (m) => {
+	const algebraCustomPluginFactory = m.contract("AlgebraCustomPluginFactory", [factoryAddress, entryPoint]);
+	
+	m.call(algebraCustomPluginFactory, "setFarmingAddress", [farmingCenter]);
+
+	return { algebraCustomPluginFactory };
+});

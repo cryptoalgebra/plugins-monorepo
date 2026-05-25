5726c3b6c613e9819f4916551841fbca0ecb23a2
Lifopam <budakovtfl@gmail.com>
2026-05-25 17:30:01 +0300

    add security registry dependency for deployment script

--- packages/default-plugin/hardhat.config.ts
+++ packages/default-plugin/hardhat.config.ts
@@ -42,6 +42,7 @@ const config: HardhatUserConfig = {
       '@cryptoalgebra/farming-proxy-plugin/contracts/FarmingProxyPluginImplementation.sol',
       '@cryptoalgebra/alm-plugin/contracts/AlmPluginImplementation.sol',
       '@cryptoalgebra/safety-switch-plugin/contracts/SecurityPluginImplementation.sol',
+      '@cryptoalgebra/safety-switch-plugin/contracts/SecurityRegistry.sol',
     ],
   },
   solidity: HIGHEST_OPTIMIZER_COMPILER_SETTINGS,

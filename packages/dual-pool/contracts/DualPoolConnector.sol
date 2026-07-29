b7d8b7b8f9702b8c94a167b08f18ffb6e7dafee7
IliaAzhel <aje-ilia2013@yandex.by>
2026-07-29 16:08:40 +0300

    add plugin config and _initializeDualPool

--- packages/dual-pool/contracts/DualPoolConnector.sol
+++ packages/dual-pool/contracts/DualPoolConnector.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.8.20;
 
+import '@cryptoalgebra/integral-core/contracts/libraries/Plugins.sol';
 import '@cryptoalgebra/integral-core/contracts/interfaces/pool/IAlgebraPoolImmutables.sol';
 import '@cryptoalgebra/abstract-plugin/contracts/BaseConnector.sol';
 import './libraries/DualPoolStorage.sol';
@@ -9,30 +10,36 @@ import './interfaces/IDualPoolPluginImplementation.sol';
 
 abstract contract DualPoolConnector is BaseConnector, IDualPoolPlugin {
   string internal constant DUAL_POOL_MODULE_NAME = 'DualPool Plugin';
+  // beforeSwap: JIT deploy, afterSwap: JIT teardown, beforeModifyPosition: block external mint/burn
+  uint8 internal constant DUAL_POOL_PLUGIN_CONFIG =
+    uint8(Plugins.BEFORE_SWAP_FLAG | Plugins.AFTER_SWAP_FLAG | Plugins.BEFORE_POSITION_MODIFY_FLAG);
 
   address internal immutable dualPoolImplementation;
-  address internal immutable dualPoolToken0;
-  address internal immutable dualPoolToken1;
 
-  constructor(address _dualPoolImplementation, address _pool) {
+  constructor(address _dualPoolImplementation) {
     dualPoolImplementation = _dualPoolImplementation;
-    dualPoolToken0 = IAlgebraPoolImmutables(_pool).token0();
-    dualPoolToken1 = IAlgebraPoolImmutables(_pool).token1();
   }
 
-  function initializeDualPool(address _vault0, address _vault1) external override {
-    _authorize();
+  // supplied by whatever contract composes this connector (e.g. UpgradeableAbstractPlugin)
+  function _getPool() internal view virtual returns (address);
+
+  function _initializeDualPool(address _vault0, address _vault1) internal {
+    address pool_ = _getPool();
     _delegateCall(
       dualPoolImplementation,
-      abi.encodeCall(IDualPoolPluginImplementation.initializeDualPool, (_vault0, _vault1, dualPoolToken0, dualPoolToken1))
+      abi.encodeCall(
+        IDualPoolPluginImplementation.initializeDualPool,
+        (_vault0, _vault1, IAlgebraPoolImmutables(pool_).token0(), IAlgebraPoolImmutables(pool_).token1())
+      )
     );
-    emit DualPoolInitialized(_vault0, _vault1);
   }
 
+  /// @inheritdoc IDualPoolPlugin
   function vault0() public view override returns (address) {
     return address(DualPoolStorage.layout().vault0);
   }
 
+  /// @inheritdoc IDualPoolPlugin
   function vault1() public view override returns (address) {
     return address(DualPoolStorage.layout().vault1);
   }
--- packages/dual-pool/contracts/interfaces/IDualPoolPlugin.sol
+++ packages/dual-pool/contracts/interfaces/IDualPoolPlugin.sol
@@ -4,8 +4,6 @@ pragma solidity >=0.5.0;
 interface IDualPoolPlugin {
   event DualPoolInitialized(address vault0, address vault1);
 
-  function initializeDualPool(address _vault0, address _vault1) external;
-
   function vault0() external view returns (address);
 
   function vault1() external view returns (address);
